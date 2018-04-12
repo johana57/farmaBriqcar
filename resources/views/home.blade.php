f9bae97b3a5f60b4c450b298910eea6cb73c2c78
Johana Rivas <jmrivas@venprecar.com>
2018-04-12 14:22:27 -0400

    Creando el repositorio farmabriqcar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('adminlte::page')
+@section('title', 'Inicio')
+
+@section('content')
+<div class="container">
+    
+        
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    
+                    @can('destroy_notes')
+                        You are logged in!
+                        {{ session('status') }}
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    @else
+                        Usted no tiene la permisologia suficiente.
+                    @endcan
+                </div>
+            </div>
+        
+    
+</div>
+@endsection
