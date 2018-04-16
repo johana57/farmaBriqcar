801ad7039943275e2315d8ddd34bf64b2dc0e08b
Johana Rivas <jmrivas@venprecar.com>
2018-04-16 09:49:34 -0400

    Modificando parametros dataBase Calidad y gitignore

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '10.50.0.134'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
