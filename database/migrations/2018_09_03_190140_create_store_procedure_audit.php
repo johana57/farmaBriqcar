159b6a625f146c2b45372cfd09a7f495279d7b6c
Johana Rivas <jmrivas@venprecar.com>
2018-09-04 15:00:45 -0400

    creando y probando procedimiento almacenado y trigger para auditar tablas

--- database/migrations/2018_09_03_190140_create_store_procedure_audit.php
+++ database/migrations/2018_09_03_190140_create_store_procedure_audit.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Auth;
 
 class CreateStoreProcedureAudit extends Migration
 {
@@ -13,20 +14,27 @@ class CreateStoreProcedureAudit extends Migration
      */
     public function up()
     {
-        DB::unprepared("
+        if(Auth::user()){
+            $id = Auth::user()->name;
+        }else{
+            
+            $id = get_current_user();
+        }
+        $result = DB::unprepared("
 CREATE OR REPLACE FUNCTION fn_log_audit() RETURNS trigger AS
 $$
+ 
 BEGIN
   IF (TG_OP = 'DELETE') THEN 
-    INSERT INTO tbl_audit VALUES (default, TG_TABLE_NAME, 'D',OLD, NULL, now(), USER);
+    INSERT INTO tbl_audit VALUES (default, TG_TABLE_NAME, 'D',OLD, NULL, now(), '$id');
     RETURN OLD;
     
   ELSIF (TG_OP = 'UPDATE') THEN
-    INSERT INTO tbl_audit VALUES (default, TG_TABLE_NAME, 'U',OLD, NULL, now(), USER);
+    INSERT INTO tbl_audit VALUES (default, TG_TABLE_NAME, 'U',OLD, NULL, now(), '$id');
     RETURN NEW;
     
   ELSIF (TG_OP = 'INSERT') THEN
-    INSERT INTO tbl_audit VALUES (default, TG_TABLE_NAME, 'I',NULL, NEW, now(), USER);
+    INSERT INTO tbl_audit VALUES (default, TG_TABLE_NAME, 'I',NULL, NEW, now(), '$id');
     RETURN NEW;
  
   END IF;
